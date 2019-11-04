ece9fdc232b6a94d54afe6d1dbe93e869ccb6d65
njwest <njwest9@gmail.com>
2019-11-04 13:52:34 +0800

    we rollin :cool:

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <CSSGrid msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+// @ is an alias to /src
+import CSSGrid from '@/layouts/CSSGrid.vue'
+
+export default {
+  name: 'home',
+  components: {
+    CSSGrid
+  }
+}
+</script>
